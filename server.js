de7af8e4b28162eeae05219025c21377bca8d645
MING CHUN KONG <mxk7199@cs.rit.edu>
2016-04-03 01:44:21 -0400

    fs.exists doesn't work lol, going back to path.exists

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var reqobj = require("request")
 var server = my_http.createServer(function(request,response){
     var my_path = url.parse(request.url).pathname;
     var full_path = path.join(process.cwd(),my_path);
-    fs.exists(full_path, function(exists){
+    path.exists(full_path, function(exists){
 	      if(!exists){
 	          response.writeHeader(404, {"Content-Type": "text/plain"});
 	          response.write("404 Not Found\n");
