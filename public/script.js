9e92172031b2f57b69313b79337749b7a5aaa943
officer <me@christran.in>
2016-04-03 09:14:30 -0400

    sdf

--- public/script.js
+++ public/script.js
@@ -55,11 +55,11 @@ window.onload = function() {
     };
     var message = JSON.stringify(json);
     socket.send(message);
-    json = {
+    var json = {
         "type": "online",
         "username": document.getElementById('loggedIn').getAttribute('uid')
     };
-    message = JSON.stringify(json);
+    var message = JSON.stringify(json);
     socket.send(message);
   // Send a message when the form is submitted.
   form.onsubmit = function(e) {
