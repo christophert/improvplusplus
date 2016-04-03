e593360cfbab42a80046f59976ede03c78a27e46
officer <me@christran.in>
2016-04-03 09:28:43 -0400

    idk

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ wsServer.on('request', function(request) {
     });
 
     connection.on('close', function(reasonCode, description) {
-        del usernames[index];
+        usernames.splice(index, 1);
         for(var i = 0; i < clients.length; i++) {
             clients[i].sendUTF(JSON.stringify({username: username, message: 'User disconnected.', date: (new Date()).getTime()}));
         }
